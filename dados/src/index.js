da1b14c3cab22d6e2e2e12404476f6d441d6d82f
hiudyy <hiudyyy@gmail.com>
2025-09-23 17:38:36 -0300

    Atualização Logica Sender

--- dados/src/index.js
+++ dados/src/index.js
@@ -1439,7 +1439,7 @@ async function NazuninhaBotExec(nazu, info, store, groupCache, messagesCache) {
     const from = info.key.remoteJid;
     const isGroup = from?.endsWith('@g.us') || false;
     if (!info.key.participant && !info.key.remoteJid) return;
-    const sender = isGroup ? info.key.participant?.includes('whatsapp.net') ? info.key.participant : info.key.participantPn : info.key.remoteJid;
+    const sender = isGroup ? info.key.participant?.includes('whatsapp.net') ? info.key.participant : info.key.participantPn ? info.key.participantPn : info.key.participantLid : info.key.remoteJid;
     const pushname = info.pushName || '';
     const isStatus = from?.endsWith('@broadcast') || false;
     const nmrdn = numerodono.replace(/[^\d]/g, "") + '@s.whatsapp.net';
