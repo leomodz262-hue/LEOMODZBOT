3a94d9a6eac26c4ec79bacc8b66e7b8d9b7c1aac
hiudyy <hiudyyy@gmail.com>
2025-10-16 15:21:03 -0300

    Update on 2025-10-16 15:21:02

--- dados/src/funcs/goldSystem.js
+++ dados/src/funcs/goldSystem.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Define ECONOMY_FILE path
-const ECONOMY_FILE = path.join(__dirname, '..', '..', 'dados', 'database', 'economy.json');
+const ECONOMY_FILE = path.join(__dirname, '..', '..', 'database', 'economy.json');
 
 function loadEconomy() {
   try {
